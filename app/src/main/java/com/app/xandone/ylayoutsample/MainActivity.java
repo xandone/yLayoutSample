50fdea18c92d60e405b28ea7148edc1d1db79bd5
xandone <765478955@qq.com>
2017-11-23 10:24:45 +0800

    init

--- app/src/main/java/com/app/xandone/ylayoutsample/MainActivity.java
+++ app/src/main/java/com/app/xandone/ylayoutsample/MainActivity.java
@@ -3,6 +3,7 @@ package com.app.xandone.ylayoutsample;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,6 +13,7 @@ import com.app.xandone.ylayoutsample.coor.CoordinatorLayoutAct;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button btn_0;
     private Button btn_1;
+    private Toolbar toolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +21,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         btn_0 = (Button) findViewById(R.id.btn_0);
         btn_1 = (Button) findViewById(R.id.btn_1);
+        toolBar = (Toolbar) findViewById(R.id.toolBar);
+
         btn_0.setOnClickListener(this);
         btn_1.setOnClickListener(this);
+
+        toolBar.setTitle("MainActivity");
     }
 
     @Override
--- app/src/main/java/com/app/xandone/ylayoutsample/coor/CoordinatorLayoutAct.java
+++ app/src/main/java/com/app/xandone/ylayoutsample/coor/CoordinatorLayoutAct.java
@@ -3,7 +3,6 @@ package com.app.xandone.ylayoutsample.coor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
